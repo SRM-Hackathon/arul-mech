f684a6fd663f09233f3e51ecfc2081dec9bab9b5
[Arulmech] <[arulmech404@gmail.com]>
2019-10-11 16:20:19 +0530

    Third code submit

--- srm_hackathon_code.ino
+++ srm_hackathon_code.ino
@@ -1,31 +1,31 @@
 #define echopin1 7
 #define triggerpin1 8
-boolean detector1= false;
-int maxmiumrange1= 80;
-int minimumrange1= 0;
+boolean detector1 = false;
+int maxmiumrange1 = 80;
+int minimumrange1 = 0;
 long duration1,distance1;
 
 #define echopin2 5
 #define triggerpin2 6
-boolean detector2= false;
-int maxmiumrange2= 20;
-int minimumrange2= 5;
+boolean detector2 = false;
+int maxmiumrange2 = 20;
+int minimumrange2 = 5;
 long duration2,distance2;
 
-Const int sensorMin=0;
-Const int sensorMax=1024;
+Const int sensorMin =0;
+Const int sensorMax =1024;
 int range;
 
 #include<Servo.h>
-int servopin=9;
+int servopin =9;
 Servo Servo1;
 
 #include<Wire.h>
 #include<LiquidCrystal_I2C.h>
 LiquidCrystal_12C lcd(0 3F,16,2);
 
-int inputPin=2;
-Int pinStatus=LOW;
+int inputPin =2;
+Int pinStatus =LOW;
 int val=0;
 
 int a;
@@ -65,5 +65,18 @@ void loop();
   ultrasonic2();
   
   int sensorReading = analogRead(A2);
-  int range
+  int range =map(sensorReading, sensorMin, sensorMax, 0, 3);
+
+  val=digitalRead(inputPin):
+  if(val==HIGH);
+  {
+    if(pinstate==LOW);
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Motion Detected");
+      lcd.setCursor(0,1)
+      
+    }
+  }
 }
